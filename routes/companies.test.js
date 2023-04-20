3963b9a41cdc0595868c944f46de191070ee20ef
alexm <alex-m@outlook.com>
2023-04-20 15:50:06 -0700

    Add test setup and teardown functions - clearing tables, adding test data, closing DB connection

--- routes/companies.test.js
+++ routes/companies.test.js
@@ -12,3 +12,39 @@ const request = require("supertest");
 
 const { app } = require("../app");
 const { db } = require("../db");
+
+let testComp01;
+
+
+beforeAll(async () => {
+
+    // Delete any data in companies table
+    await db.query(`
+        DELETE FROM companies`);
+})
+
+beforeEach(async () => {
+
+    // Add initial company data
+    const result01 = await db.query(`
+        INSERT INTO companies (code, name, description)
+        VALUES ('comp01', 'Test Company 01', 'Desc 01')
+        RETURNING code, name, description`);
+
+    testComp01 = result01.rows[0];
+})
+
+
+afterEach(async () => {
+
+    // Delete company data created by test
+    await db.query(`
+        DELETE FROM companies`);
+})
+
+
+afterAll(async () => {
+
+    // Close DB connection
+    await db.end();
+})
